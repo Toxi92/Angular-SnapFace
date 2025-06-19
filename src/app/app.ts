1ddaca749dc2f03196432aec47f3c219b639d74c
Edgar.M <146377700+Toxi92@users.noreply.github.com>
2025-06-19 15:33:56 +0200

    Header Parallaxe

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,31 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Picolo } from './picolo/picolo';
-import { PicoloClass } from './models/picoloComp';
-import { DatePipe } from '@angular/common';
+import { PicoloList } from './picolo-list/picolo-list';
+import { Header } from './header/header';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Picolo],
+  imports: [PicoloList, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 
 
-export class App implements OnInit {
-
-  getFormattedDate(): string {
-    const datePipe = new DatePipe('en-US');
-    return datePipe.transform(new Date(), 'dd/MM/yyyy HH:mm') || '';
-  }
-
-  lesPicolos!: PicoloClass[];
-
-  ngOnInit() {
-    this.lesPicolos = [ 
-    new PicoloClass('Pigeon', 'Une photo de pigeon', this.getFormattedDate(), 110, 'https://lemagdesanimaux.ouest-france.fr/images/tags/pigeon-192350.jpg', false),
-    new PicoloClass('Piccolo', 'Une photo de piccolo',this.getFormattedDate() , 200, 'https://www.fulguropop.com/wp-content/uploads/2021/10/piccolo-namek.png', false)
-    ];
-    this.lesPicolos[0].setLocalisation('Chez Zazou PRIME');
-  }
+export class App {
 }
---
+++ src/app/picolo-list/picolo-list.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Picolo } from './../picolo/picolo';
+import { PicoloClass } from './../models/picoloComp';
+import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-picolo-list',
+  imports: [Picolo],
+  templateUrl: './picolo-list.html',
+  styleUrl: './picolo-list.scss'
+})
+export class PicoloList implements OnInit {
+
+  getFormattedDate(): string {
+    const datePipe = new DatePipe('en-US');
+    return datePipe.transform(new Date(), 'dd/MM/yyyy HH:mm') || '';
+  }
+
+  lesPicolos!: PicoloClass[];
+
+  ngOnInit() {
+    this.lesPicolos = [ 
+    new PicoloClass('Pigeon', 'Une photo de pigeon', this.getFormattedDate(), 110, 'https://lemagdesanimaux.ouest-france.fr/images/tags/pigeon-192350.jpg', false),
+    new PicoloClass('Piccolo', 'Une photo de piccolo',this.getFormattedDate() , 200, 'https://www.fulguropop.com/wp-content/uploads/2021/10/piccolo-namek.png', false)
+    ];
+    this.lesPicolos[0].setLocalisation('Chez Zazou PRIME');
+  }
+}
